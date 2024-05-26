0863a66148905d4c8018710860c97a0f59465ab7
carrarook <obrunocarraro@gmail.com>
2024-05-26 15:17:58 -0300

    funcionando + css basico | a fazer: filtrar inputs, ñ deixar cep repetido e achar cep por logradouro

--- api.js
+++ api.js
@@ -1,3 +1,56 @@
-function jsApi () {
+async function jsApiCEP () {
     
+    const CEP = document.getElementById('InputCEP').value;
+    console.log(CEP);
+    const url = `https://viacep.com.br/ws/${CEP}/json/`; 
+    dado = null
+    try {
+
+    const resposta = await fetch(url);
+    const dados = await resposta.json();
+    console.log(dados); 
+    dado = dados;
+}
+    catch (error) {
+        console.error('Erro:', error);
+    }
+    
+    console.log(dado)
+    construirLista();
+    
+}
+function construirLista(){
+    const divContainer = document.getElementById('returnEND');
+    const dadosEnd = document.createElement('div');
+    dadosEnd.classList.add('returnEND');
+    dadosEnd.innerHTML = `
+        <p><strong>CEP:</strong> ${dado.cep}</p>
+        <p><strong>Logradouro:</strong> ${dado.logradouro}</p>
+        <p><strong>Complemento:</strong> ${dado.complemento ? dado.complemento : 'N/A'}</p>
+        <p><strong>Bairro:</strong> ${dado.bairro}</p>
+        <p><strong>Localidade:</strong> ${dado.localidade}</p>
+    `
+    divContainer.appendChild(dadosEnd);
+}
+function togglePlaceholderAndFunction() {
+    const inputCEP = document.getElementById('InputCEP');
+    const checkbox = document.querySelector('.switch input[type="checkbox"]');
+    const button = document.querySelector('button');
+
+    if (checkbox.checked) {
+        inputCEP.placeholder = "Insira seu Logradouro";
+        button.setAttribute('onclick', 'jsApiLog()');
+    } else {
+        inputCEP.placeholder = "Insira seu CEP";
+        button.setAttribute('onclick', 'jsApiCEP()');
+    }
+}
+
+document.querySelector('.switch input[type="checkbox"]').addEventListener('change', togglePlaceholderAndFunction);
+
+
+async function jsApiLog () {
+    // fazer dps, segundo a api vou precisar fazer um dropdown de uf -> cidade -> rua pra filtrar ex: viacep.com.br/ws/RS/Porto Alegre/Domingos/json/
+// viacep.com.br/ws/RS/Porto Alegre/Domingos Jose/json/
+// viacep.com.br/ws/RS/Porto Alegre/Domingos+Jose/json/
 }
