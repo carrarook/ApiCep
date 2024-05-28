453303bb3d37fb968ce7f3238d6abff8f05e098c
carrarook <obrunocarraro@gmail.com>
2024-05-28 18:44:55 -0300

    add Salvar ultimo endereço

--- api.js
+++ api.js
@@ -1,3 +1,4 @@
+window.onload = carregarEnderecoSalvo;  
 async function jsApiCEP () {
     
     const CEP = document.getElementById('InputCEP').value;
@@ -23,15 +24,50 @@ function construirLista(){
     const divContainer = document.getElementById('returnEND');
     const dadosEnd = document.createElement('div');
     dadosEnd.classList.add('returnEND');
+    const SalvarEnd = document.createEleme
     dadosEnd.innerHTML = `
         <p><strong>CEP:</strong> ${dado.cep}</p>
         <p><strong>Logradouro:</strong> ${dado.logradouro}</p>
         <p><strong>Complemento:</strong> ${dado.complemento ? dado.complemento : 'N/A'}</p>
         <p><strong>Bairro:</strong> ${dado.bairro}</p>
         <p><strong>Localidade:</strong> ${dado.localidade}</p>
+        <button onclick="salvarEndereco('${dado.cep}', '${dado.logradouro}', '${dado.complemento}', '${dado.bairro}', '${dado.localidade}')">Salvar Endereço</button>
     `
     divContainer.appendChild(dadosEnd);
 }
+
+function salvarEndereco(cep, logradouro, complemento, bairro, localidade) {
+    const endereco = {
+        Cep: cep,
+        Logradouro: logradouro,
+        Complemento: complemento,
+        Bairro: bairro,
+        Localidade: localidade
+    };
+
+    localStorage.setItem('ultimoEnd', JSON.stringify(endereco));
+    alert('Último endereço salvo!');
+}
+
+// Função para carregar e exibir o endereço salvo
+function carregarEnderecoSalvo() {
+    const enderecoSalvo = localStorage.getItem('ultimoEnd');
+    if (enderecoSalvo) {
+        const endereco = JSON.parse(enderecoSalvo);
+        const divContainer = document.getElementById('returnEND');
+        const dadosEnd = document.createElement('div');
+        dadosEnd.classList.add('returnEND');
+        dadosEnd.innerHTML = `
+            <p><strong>CEP:</strong> ${endereco.Cep}</p>
+            <p><strong>Logradouro:</strong> ${endereco.Logradouro}</p>
+            <p><strong>Complemento:</strong> ${endereco.Complemento ? endereco.Complemento : 'N/A'}</p>
+            <p><strong>Bairro:</strong> ${endereco.Bairro}</p>
+            <p><strong>Localidade:</strong> ${endereco.Localidade}</p>
+        `;
+        divContainer.appendChild(dadosEnd);
+    }
+}
+
 function togglePlaceholderAndFunction() {
     const inputCEP = document.getElementById('InputCEP');
     const checkbox = document.querySelector('.switch input[type="checkbox"]');
